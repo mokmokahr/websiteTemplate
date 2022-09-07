d0f3db4eb13de3adcd7b49d6069abbf1b5c06fbe
mokmokahr <jbmok.workspace@gmail.com>
2022-09-07 11:53:29 +0900

    FIX: Fix the scrollup effect

--- javascript/script.js
+++ javascript/script.js
@@ -2,16 +2,14 @@ alert("helloworld");
 
 function changeColorDetectingScroll(){
 	console.log(window.scrollY);
-	window.addEventListener('scroll',(ev)=>{
-		if(30 < window.scrollY){
-			document.getElementById("main-session").style.backgroundColor = "white";
-			document.getElementById("big-font").style.color = "rgb(0, 123, 255)";
-		}
-		else{
-			document.getElementById("main-session").style.backgroundColor = "rgb(0, 123, 255);";
-			document.getElementById("big-font");
-		}
-	});
+	if(80 < window.scrollY){
+		document.getElementById("main-session").style.backgroundColor = "white";
+		document.getElementById("big-font").style.color = "rgb(0, 123, 255)";
+	}
+	else{
+		document.getElementById("main-session").style.backgroundColor = "rgb(0, 123, 255)";
+		document.getElementById("big-font").style.color = "white";
+	}
 }
 
 setInterval(changeColorDetectingScroll,10);
